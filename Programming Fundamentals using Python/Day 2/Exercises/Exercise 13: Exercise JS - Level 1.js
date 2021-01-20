41d61dbb2a3fb6dfb0d78559f8bdcee309263d53
Ashish Maheshwari <53733415+AshishMaheshwari5959@users.noreply.github.com>
2021-01-20 18:17:27 +0530

    Create Exercise 13: Exercise JS - Level 1.js

---
+++ Programming Fundamentals using Python/Day 2/Exercises/Exercise 13: Exercise JS - Level 1.js
@@ -0,0 +1,20 @@
+///
+Write a JavaScript program to display the sum of two given numbers if the numbers are same. If not, display double the sum.
+
+Test your code by using the given sample input.
+Sample Input	Expected Output
+5,5	10
+///
+
+// Solution
+
+//PF-Exer-13
+num1=5
+num2=5
+
+//Write your code here
+if (num1 == num2) {
+    console.log(num1*2 )
+} else {
+    console.log((num1+num2)*2)
+}
