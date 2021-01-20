f0c69c95ae35416a7fe221c323b7ba8ea4fa897c
Ashish Maheshwari <53733415+AshishMaheshwari5959@users.noreply.github.com>
2021-01-20 18:36:00 +0530

    Create Exercise 14: Collaborative Exercise - Level 2.py

---
+++ Programming Fundamentals using Python/Day 2/Exercises/Exercise 14: Collaborative Exercise - Level 2.py
@@ -0,0 +1,27 @@
+'''
+A teacher in a school wants to find and display the grade of a student based on his/her percentage score. The criterion for grades is as given below:
+
+Score (both inclusive)	Grade
+Between 80 and 100	A
+Between 73 and 79	B
+Between 65 and 72	C
+Between 0 and 64	D
+Any other value	Z
+Assume that the percentage score is a whole number.
+
+Write a python program for the above requirement. Identify the test data and use it to test the program.
+'''
+
+#Solution
+
+score = 43
+if (80<= score <=100):
+    print("A")
+elif (73<= score <=79):
+    print("B")
+elif (65<= score <=72):
+    print("C")
+elif (0<= score <=64):
+    print("D")
+else :
+    print("Z")
