b0049364473043cbc936d2f80fc0d12827be9050
Ashish Maheshwari <53733415+AshishMaheshwari5959@users.noreply.github.com>
2021-01-21 18:07:25 +0530

    Create Exercise 18 - Level 2.py

---
+++ Programming Fundamentals using Python/Day 3/Exercises/Exercise 18 - Level 2.py
@@ -0,0 +1,26 @@
+'''
+Given a list of integer values. Write a python program to check whether it contains same number in adjacent position. Display the count of such adjacent occurrences.
+Estimated Time: 30 minutes
+
+Sample Input	Expected Output
+[1,1,5,100,-20,-20,6,0,0]	3
+[10,20,30,40,30,20]	0
+[1,2,2,3,4,4,4,10]	3
+'''
+
+# Solution
+
+#PF-Exer-18
+
+def get_count(num_list):
+    count=0
+    for i  in range(0,len(num_list)-1):
+        if num_list[i] == num_list[i+1] :
+            count = count + 1
+    # Write your logic here
+
+    return count
+
+#provide different values in list and test your program
+num_list=[1,1,5,100,-20,-20,6,0,0]
+print(get_count(num_list))
